b5ea5772140c9849c1f492d18f7dbee823d1443c
Polex Ouma <106394541+ouma999@users.noreply.github.com>
2026-03-02 10:53:01 -0600

    Add files via upload

---
+++ ETL-Retail_sales.sql
@@ -0,0 +1,103 @@
+CREATE DATABASE RetailETL;
+GO
+
+USE RetailETL;
+GO
+SELECT TOP 10 * FROM retail_sales_dataset;
+SELECT COUNT(*) FROM retail_sales_dataset;
+/*check for null value */
+SELECT *
+FROM retail_sales_dataset
+WHERE transaction_id IS NULL
+   OR Date IS NULL
+   OR customer_id IS NULL;
+/*checks for duplicates*/
+SELECT transaction_id, COUNT(*) AS duplicate_count
+FROM retail_sales_dataset
+GROUP BY transaction_id
+HAVING COUNT(*) > 1
+
+/*Create Data Warehouse Structure to give us a star schema 
+We will build:The smaller tables called dimension table that surround the fact table(main table from the dataset) 
+dim_customer
+dim_product
+dim_date
+fact_sales*/
+
+/*I designed a star schema in SQL Server, created dimension tables for customers, products, 
+and dates, and loaded a fact table using surrogate keys and foreign key relationships.*/
+CREATE TABLE dim_customer (
+    customer_key INT IDENTITY(1,1) PRIMARY KEY,
+    customer_id NVARCHAR(50),
+    gender NVARCHAR(50),
+    age INT
+);
+
+INSERT INTO dim_customer (customer_id, gender, age)
+SELECT DISTINCT
+    Customer_ID,
+    Gender,
+    Age
+FROM retail_sales_dataset;
+
+CREATE TABLE dim_product (
+    product_key INT IDENTITY(1,1) PRIMARY KEY,
+    product_category NVARCHAR(100)
+);
+INSERT INTO dim_product (product_category)
+SELECT DISTINCT Product_Category
+FROM retail_sales_dataset;
+
+
+CREATE TABLE dim_date (
+    date_key INT PRIMARY KEY,
+    full_date DATE,
+    year INT,
+    month INT,
+    day INT
+);
+INSERT INTO dim_date (date_key, full_date, year, month, day)
+SELECT DISTINCT
+    CONVERT(INT, FORMAT(Date, 'yyyyMMdd')) AS date_key,
+    Date,
+    YEAR(Date),
+    MONTH(Date),
+    DAY(Date)
+FROM retail_sales_dataset;
+
+CREATE TABLE fact_sales (
+    sales_key INT IDENTITY(1,1) PRIMARY KEY,
+    transaction_id SMALLINT,
+    customer_key INT,
+    product_key INT,
+    date_key INT,
+    quantity INT,
+    price_per_unit SMALLINT,
+    total_amount SMALLINT,
+    FOREIGN KEY (customer_key) REFERENCES dim_customer(customer_key),
+    FOREIGN KEY (product_key) REFERENCES dim_product(product_key),
+    FOREIGN KEY (date_key) REFERENCES dim_date(date_key)
+);
+INSERT INTO fact_sales (
+    transaction_id,
+    customer_key,
+    product_key,
+    date_key,
+    quantity,
+    price_per_unit,
+    total_amount
+)
+SELECT
+    r.Transaction_ID,
+    c.customer_key,
+    p.product_key,
+    CONVERT(INT, FORMAT(r.Date, 'yyyyMMdd')) AS date_key,
+    r.Quantity,
+    r.Price_Per_Unit,
+    r.Total_Amount
+FROM retail_sales_dataset r
+JOIN dim_customer c
+    ON r.Customer_ID = c.customer_id
+JOIN dim_product p
+    ON r.Product_Category = p.product_category;
+
